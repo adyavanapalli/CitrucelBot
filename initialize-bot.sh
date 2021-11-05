1cd0f7a8990565ce80f52df4430a3a8235949c5f
Anand Dyavanapalli <adyavanapalli@gmail.com>
2021-11-05 09:51:32 -0400

    refactor: Consolidate gcloud commands into bash functions.

--- initialize-bot.sh
+++ initialize-bot.sh
@@ -7,7 +7,8 @@
 #        topic.
 #     3. A Cloud Tasks queue.
 #     4. A Secret Manager secret key.
-#     5. A service account with the Cloud Tasks Enqueuer role.
+#     5. A service account with the Cloud Tasks Enqueuer and Secret Manager
+#        Secret Accessor roles.
 #     6. A Cloud Functions function that triggers when messages are published to
 #        the aforementioned topic and creates a Cloud Tasks task in the
 #        aforementioned queue.
@@ -19,39 +20,62 @@ set -o posix
 set -o xtrace
 
 
-gcloud pubsub topics create citrucel-topic
+create_pubsub_topic() {
+    gcloud pubsub topics create citrucel-topic
+}
 
-gcloud scheduler jobs create pubsub citrucel-job \
-    --message-body="citrucel-job" \
-    --schedule="0 18 * * *" \
-    --time-zone="America/New_York" \
-    --topic="citrucel-topic"
+create_scheduler_job() {
+    gcloud scheduler jobs create pubsub citrucel-job \
+        --message-body="citrucel-job" \
+        --schedule="0 18 * * *" \
+        --time-zone="America/New_York" \
+        --topic="citrucel-topic"
+}
 
-gcloud tasks queues create citrucel-queue \
-    --log-sampling-ratio="1.0"
+create_task_queue() {
+    gcloud tasks queues create citrucel-queue \
+        --log-sampling-ratio="1.0"
+}
 
-echo -n "$CITRUCEL_BOT_TOKEN" | gcloud secrets create citrucel-secret \
-    --data-file="-"
+create_secret_manager_key() {
+    echo -n "$CITRUCEL_BOT_TOKEN" | gcloud secrets create citrucel-secret \
+        --data-file="-"
+}
 
-gcloud iam service-accounts create citrucel-sa
+create_service_account() {
+    gcloud iam service-accounts create citrucel-sa
 
-gcloud projects add-iam-policy-binding citrucelbot \
-    --member="serviceAccount:citrucel-sa@citrucelbot.iam.gserviceaccount.com" \
-    --role="roles/cloudtasks.enqueuer"
+    gcloud projects add-iam-policy-binding citrucelbot \
+        --member="serviceAccount:citrucel-sa@citrucelbot.iam.gserviceaccount.com" \
+        --role="roles/cloudtasks.enqueuer"
 
-gcloud projects add-iam-policy-binding citrucelbot \
-    --member='serviceAccount:citrucel-sa@citrucelbot.iam.gserviceaccount.com' \
-    --role='roles/secretmanager.secretAccessor'
+    gcloud projects add-iam-policy-binding citrucelbot \
+        --member='serviceAccount:citrucel-sa@citrucelbot.iam.gserviceaccount.com' \
+        --role='roles/secretmanager.secretAccessor'
+}
 
-gcloud beta functions deploy citrucel-function \
-    --env-vars-file="env/citrucel-function.yaml" \
-    --entry-point="Citrucel.Function" \
-    --ingress-settings="internal-only" \
-    --max-instances="1" \
-    --memory="128MB" \
-    --runtime="dotnet3" \
-    --service-account="citrucel-sa@citrucelbot.iam.gserviceaccount.com" \
-    --region="us-east1" \
-    --set-secrets "BOT_TOKEN=citrucel-secret:latest" \
-    --source="Citrucel" \
-    --trigger-topic="citrucel-topic"
+create_cloud_function() {
+    gcloud beta functions deploy citrucel-function \
+        --env-vars-file="env/citrucel-function.yaml" \
+        --entry-point="Citrucel.Function" \
+        --ingress-settings="internal-only" \
+        --max-instances="1" \
+        --memory="128MB" \
+        --runtime="dotnet3" \
+        --service-account="citrucel-sa@citrucelbot.iam.gserviceaccount.com" \
+        --region="us-east1" \
+        --set-secrets "BOT_TOKEN=citrucel-secret:latest" \
+        --source="Citrucel" \
+        --trigger-topic="citrucel-topic"
+}
+
+main() {
+    create_pubsub_topic
+    create_scheduler_job
+    create_task_queue
+    create_secret_manager_key
+    create_service_account
+    create_cloud_function
+}
+
+main
