1238d639de634895ba7df53c3df00b8e946c532c
Anand Dyavanapalli <adyavanapalli@gmail.com>
2022-01-17 18:37:08 -0500

    feat: Add Terraform configuration.

---
+++ .terraform.lock.hcl
@@ -0,0 +1,57 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/arthurhlt/zipper" {
+  version = "0.14.0"
+  hashes = [
+    "h1:5BdJd/Ihh4QIPQ8ZFiQF725/A0k6Ne5769/Jln0N+dE=",
+    "zh:26ff6c21f9a88e10c81529aae9f299ace11ea7206187b42d5fc3d0c35676244d",
+    "zh:2ecbf8a992c5008d294b77a49df8ebb3489057cf2ad0a9875d444b2342eed394",
+    "zh:6439f64a1cec4f025c2c31885e80185ee2fae299252ee76e42f305c1b6403c56",
+    "zh:8accc9b1df5087787c2e75a6baa283c0a8104c1c62800eefd76b73a7210bb4cb",
+    "zh:8c46265317d8d43f657a1798e0f2c64d3c5e75471b5aefce99aac349d51a8282",
+    "zh:91fff22171a405a8948a112959d7250b7431158b0fc649fbbb5f2e4d5662a744",
+    "zh:a4dd79081ed3bf49fc42f289419bf2b8087e3644c33bd5714a60e2a543480e6d",
+    "zh:ac404943f401d452d644b739eadb54c891387378b73fb8d8ff336aa4c37d0001",
+    "zh:b7ce5c3f6d4a4c16f09158357768c608046c9a3f9c84bff9ce24bfac4fbca6f3",
+    "zh:ba6e795abf4db5220fa65e6729b1042d320f1576f8d34359a784c7b5e1baf3b4",
+    "zh:d96c481e1ffe5d661276bf87482bc171fabaac74962bf74c11ee1990837648ea",
+    "zh:e2e87ff8e81e66292373c786562c5a36866d04d3d5c0f93020dfa3a3fda135fd",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/google" {
+  version = "4.6.0"
+  hashes = [
+    "h1:QbO4yjDrnoSpiYKSHrICNL1ZuWsl5J2rVRFj2kNg7xA=",
+    "zh:005a28a2c79f6b29680b0f57260c69c85d8a992688007b6e5645149bd379951f",
+    "zh:2604d825de72cf99b4899d7880837adeb19d371f48e419666e32c4c3cf6a72e9",
+    "zh:290da4eb18e44469480cf299bebce89f54e4d301f856cdffe2837b498878c7ec",
+    "zh:3e5ba1a55d38fa17533a18fc14a612e781ded76c6309734d3dc0a937be27eec1",
+    "zh:4a85de3cdb33c092d8ccfced3d7302934de0dd4f72bbcebd79d45afe0a0b6f85",
+    "zh:5fb1a79800833ae922aaba594a8b2bc83be1d254052e12e0ce8330ca0d8933d9",
+    "zh:679b9f50c6fe0476e74d37935f7598d46d6e9612f75b26a8ef1ca3c13144d06a",
+    "zh:893216e32378839668c51ef135af1676cd887d63e2edb6625cf9adad7bfa346f",
+    "zh:ad8f2fd19adbe4c10281ba9b3c8d5100877a9c541d3580bbbe9357714aa77619",
+    "zh:bff5d6fd15e98c12ee9ed98b0338761dc4a9ba671a37834926daeabf73c71783",
+    "zh:debdf15fbed8d63e397cd004bf65586bd2b93ce04e47ca51a7c70c1fe9168b87",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/random" {
+  version = "3.1.0"
+  hashes = [
+    "h1:BZMEPucF+pbu9gsPk0G0BHx7YP04+tKdq2MrRDF1EDM=",
+    "zh:2bbb3339f0643b5daa07480ef4397bd23a79963cc364cdfbb4e86354cb7725bc",
+    "zh:3cd456047805bf639fbf2c761b1848880ea703a054f76db51852008b11008626",
+    "zh:4f251b0eda5bb5e3dc26ea4400dba200018213654b69b4a5f96abee815b4f5ff",
+    "zh:7011332745ea061e517fe1319bd6c75054a314155cb2c1199a5b01fe1889a7e2",
+    "zh:738ed82858317ccc246691c8b85995bc125ac3b4143043219bd0437adc56c992",
+    "zh:7dbe52fac7bb21227acd7529b487511c91f4107db9cc4414f50d04ffc3cab427",
+    "zh:a3a9251fb15f93e4cfc1789800fc2d7414bbc18944ad4c5c98f466e6477c42bc",
+    "zh:a543ec1a3a8c20635cf374110bd2f87c07374cf2c50617eee2c669b3ceeeaa9f",
+    "zh:d9ab41d556a48bd7059f0810cf020500635bfc696c9fc3adab5ea8915c1d886b",
+    "zh:d9e13427a7d011dbd654e591b0337e6074eef8c3b9bb11b2e39eaaf257044fd7",
+    "zh:f7605bd1437752114baf601bdf6931debe6dc6bfe3006eb7e9bb9080931dca8a",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,149 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    zipper = {
+      source = "ArthurHlt/zipper"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "random_pet" "google_pubsub_topic" {
+}
+
+resource "random_pet" "google_cloud_scheduler_job" {
+}
+
+resource "random_pet" "google_cloud_tasks_queue" {
+}
+
+resource "random_pet" "google_storage_bucket" {
+}
+
+resource "random_pet" "google_cloudfunctions_function" {
+}
+
+resource "random_pet" "google_service_account" {
+}
+
+resource "google_project_service" "cloudbuild_service" {
+  service                    = "cloudbuild.googleapis.com"
+  disable_dependent_services = true
+}
+
+resource "google_project_service" "cloudfunctions_service" {
+  service                    = "cloudfunctions.googleapis.com"
+  disable_dependent_services = true
+}
+
+resource "google_project_service" "cloudscheduler_service" {
+  service                    = "cloudscheduler.googleapis.com"
+  disable_dependent_services = true
+}
+
+resource "google_project_service" "cloudtasks_service" {
+  service                    = "cloudtasks.googleapis.com"
+  disable_dependent_services = true
+}
+
+resource "google_service_account" "service_account" {
+  account_id = random_pet.google_service_account.id
+}
+
+resource "google_project_iam_binding" "iam_binding" {
+  project = var.project
+  role    = "roles/cloudtasks.enqueuer"
+
+  members = [
+    "serviceAccount:${google_service_account.service_account.email}"
+  ]
+}
+
+resource "google_pubsub_topic" "topic" {
+  name = random_pet.google_pubsub_topic.id
+}
+
+resource "google_cloud_scheduler_job" "job" {
+  name = random_pet.google_cloud_scheduler_job.id
+
+  pubsub_target {
+    topic_name = google_pubsub_topic.topic.id
+
+    data = base64encode("GNU Terry Pratchett")
+  }
+  schedule  = "0 18 * * *"
+  time_zone = "America/New_York"
+
+  depends_on = [
+    google_project_service.cloudscheduler_service
+  ]
+}
+
+resource "google_cloud_tasks_queue" "queue" {
+  location = var.region
+
+  name = random_pet.google_cloud_tasks_queue.id
+  stackdriver_logging_config {
+    sampling_ratio = 1.0
+  }
+
+  depends_on = [
+    google_project_service.cloudtasks_service
+  ]
+}
+
+resource "google_storage_bucket" "bucket" {
+  name     = random_pet.google_storage_bucket.id
+  location = var.region
+}
+
+resource "zipper_file" "fixture" {
+  output_path = "Citrucel.zip"
+  source      = "Citrucel"
+  type        = "local"
+}
+
+resource "google_storage_bucket_object" "object" {
+  name   = zipper_file.fixture.output_path
+  bucket = google_storage_bucket.bucket.name
+  source = zipper_file.fixture.output_path
+}
+
+resource "google_cloudfunctions_function" "function" {
+  name    = random_pet.google_cloudfunctions_function.id
+  runtime = "dotnet3"
+
+  available_memory_mb = 128
+  entry_point         = "Citrucel.Function"
+  environment_variables = {
+    CHAT_ID     = var.chat_id
+    LOCATION_ID = var.region
+    PROJECT_ID  = var.project
+    QUEUE_ID    = google_cloud_tasks_queue.queue.name
+    BOT_TOKEN   = var.bot_token
+  }
+  event_trigger {
+    event_type = "google.pubsub.topic.publish"
+    resource   = google_pubsub_topic.topic.id
+  }
+  ingress_settings      = "ALLOW_INTERNAL_ONLY"
+  max_instances         = 1
+  service_account_email = google_service_account.service_account.email
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.object.name
+
+  depends_on = [
+    google_project_service.cloudbuild_service,
+    google_project_service.cloudfunctions_service
+  ]
+}
